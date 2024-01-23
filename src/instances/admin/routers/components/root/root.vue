a8b0f560f353badcf418c79cdafd64dd377573e5
HAN <HAN>
2024-01-23 14:45:04 +0800

    主题调整7

--- src/instances/admin/routers/components/root/root.vue
+++ src/instances/admin/routers/components/root/root.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import RouterTransition from './transition.vue';
 defineOptions({
   name: 'RootRouterView',
 });
 </script>
 <template>
-  <router-view v-slot="{ Component }">
-    <Suspense>
-      <RouterTransition>
-        <component :is="Component" />
-      </RouterTransition>
-      <!-- 加载中状态 -->
-      <template #fallback> 正在加载... </template>
-    </Suspense>
-  </router-view>
+  <router-view></router-view>
 </template>
