57af33e92e779368c1d7ad1848a5bf3e59c7e29e
HAN <1305544590@qq.com>
2024-09-02 22:59:32 +0800

    refactor(admin): 使用设备类型枚举和响应式布局
    -用响应式挂钩替换手动组件切换，基于窗口大小动态调整布局。- 枚举设备类型（桌面、平板、手机），提高代码可读性和可维护性。
    - 在`index.ts`中引入`useLayoutType`，封装布局逻辑，促进组件响应式更新。
    
    依赖更新:
    
    - 升级`axios`至1.7.7，以修复潜在的安全漏洞。
    - 更新`@types/node`至22.5.2，与`axios`更新保持一致。
    - 其他依赖锁定版本更新，确保项目依赖一致性。
    
    BREAKING CHANGE:
    
    布局组件现在通过`useLayoutType`自动选择，而不是手动切换。这要求在使用布局组件时采用新的响应式方式。

--- src/instances/admin/App.vue
+++ src/instances/admin/App.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import type { componentsMapKey } from '@admin/layout';
-import { componentMap } from '@admin/layout';
-import { ref, unref } from 'vue';
+import { useLayoutType } from '@admin/layout';
 import { createThemeConfigProvider } from '@/theme';
 
 defineOptions({
   name: 'APP',
 });
-const currentComponentMapKey = ref<componentsMapKey>('default-desktop');
-const component = componentMap.get(unref(currentComponentMapKey));
+const { currentLayout } = useLayoutType();
 const ThemeProvider = createThemeConfigProvider();
 </script>
 <template>
   <ThemeProvider>
-    <component :is="component"></component>
+    <component :is="currentLayout"></component>
   </ThemeProvider>
 </template>
--- src/instances/admin/layout/index.ts
+++ src/instances/admin/layout/index.ts
@@ -1,21 +1,69 @@
 import type { Component } from 'vue';
-import { defineAsyncComponent } from 'vue';
+import { computed, defineAsyncComponent, unref } from 'vue';
+import { useWindowSize } from '@vueuse/core';
+
+// enum 枚举组件类型
+export enum DeviceType {
+  Desktop = 'desktop',
+  Phone = 'phone',
+  Tablet = 'tablet',
+}
 
 export type componentsMapKey =
-  | 'default-desktop'
-  | 'default-phone'
-  | 'default-tablet';
+  | DeviceType.Desktop
+  | DeviceType.Phone
+  | DeviceType.Tablet;
 
 export const componentMap: Map<componentsMapKey, Component> = new Map();
 componentMap.set(
-  'default-desktop',
+  DeviceType.Desktop,
   defineAsyncComponent(() => import('./desktop/index.vue')),
 );
 componentMap.set(
-  'default-phone',
+  DeviceType.Phone,
   defineAsyncComponent(() => import('./phone/index.vue')),
 );
 componentMap.set(
-  'default-tablet',
+  DeviceType.Tablet,
   defineAsyncComponent(() => import('./tablet/index.vue')),
 );
+
+// 屏幕大于1200 认为是pc端
+// 屏幕大于768 认为是平板
+// 屏幕小于768 认为是手机端
+export const getDeviceType = (width: number) => {
+  if (width > 1200) return DeviceType.Desktop;
+  if (width > 768) return DeviceType.Tablet;
+  return DeviceType.Phone;
+};
+
+export function useLayoutType() {
+  const { width } = useWindowSize();
+  const currentDeviceType = computed(() => {
+    return getDeviceType(unref(width));
+  });
+  const currentLayout = computed(() => {
+    return componentMap.get(unref(currentDeviceType));
+  });
+  return { currentLayout };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
